11d46fc31939696bd2dfe10c2b2a1dec2cf4f03e
DinoPranjic <dinopranjic92@gmail.com>
2022-04-14 00:42:45 +0000

    add parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
+/*
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -22,3 +22,25 @@ ORDER BY teacher;
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+*/
+
+// PARAMETERIZED 
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
+const cohort = [process.argv[2]];
+
+pool.query(queryString, cohort)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
